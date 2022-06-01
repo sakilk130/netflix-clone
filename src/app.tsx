fbc2d920b118d69131f9e6e50e9345a0e490acce
sakil.khan <sakil.khan@shebatech.com.bd>
2022-06-01 22:20:31 +0600

    feat: :sparkles: add routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,17 @@
-import FaqsContainer from "./containers/faqs";
-import FooterContainer from "./containers/footer";
-import JumbotronContainer from "./containers/jumbotron";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import { Browse, Home, SignIn, SignUp } from "./pages";
 
 function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.BROWSE} element={<Browse />} />
+        <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+        <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
