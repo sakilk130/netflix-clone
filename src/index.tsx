7996656e5cf1344f7c95f1f247d68da9589d35bc
sakil.khan <sakil.khan@shebatech.com.bd>
2022-06-02 01:45:02 +0600

    feat: :sparkles: firebase setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,21 @@ import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
+import { FirebaseContext } from "./context/firebase";
 import { GlobalStyles } from "./global-styles";
 import reportWebVitals from "./reportWebVitals";
+import { firebase } from "./lib/firebase.prod";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>
 );
 reportWebVitals();
