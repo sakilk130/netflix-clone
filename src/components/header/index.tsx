09b6557d9455958f3cc17a2d73f6ed6916b80977
sakil.khan <sakil.khan@shebatech.com.bd>
2022-06-05 00:37:27 +0600

    feat: :sparkles: create browse page header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,49 @@
+import { useState } from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
-import { Background, ButtonLink, Container, Logo } from "./styles/header";
+import {
+  Background,
+  ButtonLink,
+  Container,
+  Logo,
+  Feature,
+  Text,
+  Link,
+  FeatureCallOut,
+  Group,
+  Profile,
+  Picture,
+  Dropdown,
+  Search,
+  SearchIcon,
+  SearchInput,
+  PlayButton,
+} from "./styles/header";
 
 const Header = ({ bg = true, children, ...restProps }: any) => {
   return bg ? <Background {...restProps}>{children}</Background> : children;
 };
 
-Header.Frame = function HeaderFrame({ children, ...restProps }: any) {
+Header.Frame = ({ children, ...restProps }: any) => {
   return <Container {...restProps}>{children}</Container>;
 };
 
-Header.Logo = function HeaderLogo({ to, ...restProps }: any) {
+Header.Profile = ({ children, ...restProps }: any) => {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Picture = ({ src, ...restProps }: any) => {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
+Header.Dropdown = ({ children, ...restProps }: any) => {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
+Header.Group = ({ children, ...restProps }: any) => {
+  return <Group {...restProps}>{children}</Group>;
+};
+
+Header.Logo = ({ to, ...restProps }: any) => {
   return (
     <ReactRouterLink to={to}>
       <Logo {...restProps} />
@@ -17,8 +51,53 @@ Header.Logo = function HeaderLogo({ to, ...restProps }: any) {
   );
 };
 
-Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }: any) {
+Header.Search = function HeaderSearch({
+  searchTerm,
+  setSearchTerm,
+  ...restProps
+}: any) {
+  const [searchActive, setSearchActive] = useState(false);
+
+  return (
+    <Search {...restProps}>
+      <SearchIcon
+        onClick={() => setSearchActive((searchActive) => !searchActive)}
+      >
+        <img src="/images/icons/search.png" alt="Search" />
+      </SearchIcon>
+      <SearchInput
+        value={searchTerm}
+        onChange={({ target }) => setSearchTerm(target.value)}
+        placeholder="Search films and series"
+        active={searchActive}
+        data-testid="search-input"
+      />
+    </Search>
+  );
+};
+
+Header.Feature = ({ children, ...restProps }: any) => {
+  return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.Text = ({ children, ...restProps }: any) => {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.TextLink = ({ children, ...restProps }: any) => {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Header.FeatureCallOut = ({ children, ...restProps }: any) => {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.ButtonLink = ({ children, ...restProps }: any) => {
   return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
+
+Header.PlayButton = ({ children, ...restProps }: any) => {
+  return <PlayButton {...restProps}>{children}</PlayButton>;
 };
 
 export default Header;
--- src/containers/browse.tsx
+++ src/containers/browse.tsx
@@ -2,13 +2,18 @@ import { useContext, useEffect, useState } from "react";
 import { Header, Loading } from "../components";
 import { FirebaseContext } from "../context/firebase";
 import SelectProfileContainer from "./profile";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
 
 const BrowseContainer = ({ slides }: any) => {
   const [profile, setProfile] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  console.log(profile);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
+
+  console.log(profile);
 
   useEffect(() => {
     setTimeout(() => {
@@ -20,7 +25,43 @@ const BrowseContainer = ({ slides }: any) => {
     <>
       {loading ? <Loading src={profile?.photoURL} /> : null}
       <Header src="joker1">
-        <h1>Hello</h1>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+            <Header.TextLink>Series</Header.TextLink>
+            <Header.TextLink>Films</Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Search
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+            />
+            <Header.Profile>
+              <Header.Picture src={profile?.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={profile?.photoURL} />
+                  <Header.TextLink>{user?.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                    Sign out
+                  </Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+            cumque, itaque, mollitia ad necessitatibus animi eligendi sint
+            officiis amet voluptatibus repellat dolorum. Dolor deserunt,
+            mollitia nobis quas incidunt magnam doloremque!
+          </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
+        </Header.Feature>
       </Header>
     </>
   ) : (
