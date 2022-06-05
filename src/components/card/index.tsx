27d91ce680e6f27f0d3c6d149ad4437cbf7b4e6e
sakil.khan <sakil.khan@shebatech.com.bd>
2022-06-06 01:33:32 +0600

    feat: :sparkles: browse page card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,120 @@
+import { createContext, useState, useContext } from "react";
+import {
+  Container,
+  Content,
+  Entities,
+  Feature,
+  FeatureClose,
+  FeatureText,
+  FeatureTitle,
+  Group,
+  Image,
+  Item,
+  Maturity,
+  Meta,
+  SubTitle,
+  Text,
+  Title,
+} from "./styles/card";
+
+interface IFeatureProvider {
+  showFeature: boolean;
+  setShowFeature: (value: any) => void;
+  itemFeature: any;
+  setItemFeature: (value: any) => void;
+}
+
+const FeatureContext = createContext<IFeatureProvider>({
+  showFeature: false,
+  setShowFeature: () => {},
+  itemFeature: {},
+  setItemFeature: () => {},
+});
+
+const Card = ({ children, ...restProps }: any) => {
+  const [showFeature, setShowFeature] = useState(false);
+  const [itemFeature, setItemFeature] = useState({});
+
+  return (
+    <FeatureContext.Provider
+      value={{ showFeature, setShowFeature, itemFeature, setItemFeature }}
+    >
+      <Container {...restProps}>{children}</Container>
+    </FeatureContext.Provider>
+  );
+};
+
+Card.Group = function CardGroup({ children, ...restProps }: any) {
+  return <Group {...restProps}>{children}</Group>;
+};
+
+Card.Title = function CardTitle({ children, ...restProps }: any) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Card.SubTitle = function CardSubTitle({ children, ...restProps }: any) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Card.Text = function CardText({ children, ...restProps }: any) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Card.Item = function CardItem({ item, children, ...restProps }: any) {
+  const { setItemFeature, setShowFeature } = useContext(FeatureContext);
+  return (
+    <Item
+      onClick={() => {
+        setItemFeature(item);
+        setShowFeature(true);
+      }}
+      {...restProps}
+    >
+      {children}
+    </Item>
+  );
+};
+
+Card.Entities = function CardEntities({ children, ...restProps }: any) {
+  return <Entities {...restProps}>{children}</Entities>;
+};
+
+Card.Meta = function CardMeta({ children, ...restProps }: any) {
+  return <Meta {...restProps}>{children}</Meta>;
+};
+
+Card.Image = function CardImage({ ...restProps }: any) {
+  return <Image {...restProps} />;
+};
+
+Card.Feature = function CardFeature({ category, children, ...restProps }: any) {
+  const { itemFeature, setItemFeature, setShowFeature, showFeature } =
+    useContext(FeatureContext);
+
+  return showFeature ? (
+    <Feature
+      {...restProps}
+      src={`/images/${category}/${itemFeature?.genre}/${itemFeature?.slug}/large.jpg`}
+    >
+      <Content>
+        <FeatureTitle>{itemFeature?.title}</FeatureTitle>
+        <FeatureText>{itemFeature?.description}</FeatureText>
+        <FeatureClose onClick={() => setShowFeature(false)}>
+          <img src="/images/icons/close.png" alt="Close" />
+        </FeatureClose>
+      </Content>
+      <Group margin="30px 0px" flexDirection="row" alignsItems="center">
+        <Maturity rating={itemFeature.maturity}>
+          {itemFeature.maturity < 12 ? "PG" : itemFeature.maturity}
+        </Maturity>
+        <FeatureText fontWeight="bold">
+          {itemFeature.genre.charAt(0).toUpperCase() +
+            itemFeature.genre.slice(1)}
+        </FeatureText>
+      </Group>
+      {children}
+    </Feature>
+  ) : null;
+};
+
+export default Card;
--- src/containers/browse.tsx
+++ src/containers/browse.tsx
@@ -1,14 +1,16 @@
 import { useContext, useEffect, useState } from "react";
-import { Header, Loading } from "../components";
+import { Card, Header, Loading } from "../components";
 import { FirebaseContext } from "../context/firebase";
 import SelectProfileContainer from "./profile";
 import * as ROUTES from "../constants/routes";
 import logo from "../logo.svg";
 
 const BrowseContainer = ({ slides }: any) => {
+  const [category, setCategory] = useState("series");
   const [profile, setProfile] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [slideRows, setSlideRows] = useState<any>([]);
 
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
@@ -21,6 +23,10 @@ const BrowseContainer = ({ slides }: any) => {
     }, 3000);
   }, [profile?.displayName]);
 
+  useEffect(() => {
+    setSlideRows(slides[category]);
+  }, [category, slides]);
+
   return profile?.displayName ? (
     <>
       {loading ? <Loading src={profile?.photoURL} /> : null}
@@ -28,8 +34,18 @@ const BrowseContainer = ({ slides }: any) => {
         <Header.Frame>
           <Header.Group>
             <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-            <Header.TextLink>Series</Header.TextLink>
-            <Header.TextLink>Films</Header.TextLink>
+            <Header.TextLink
+              active={category === "series" ? true : false}
+              onClick={() => setCategory("series")}
+            >
+              Series
+            </Header.TextLink>
+            <Header.TextLink
+              active={category === "films" ? true : false}
+              onClick={() => setCategory("films")}
+            >
+              Films
+            </Header.TextLink>
           </Header.Group>
           <Header.Group>
             <Header.Search
@@ -63,6 +79,30 @@ const BrowseContainer = ({ slides }: any) => {
           <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
+
+      <Card.Group>
+        {slideRows.map((slideItem: any) => (
+          <Card key={`${category}-${slideItem?.title?.toLowerCase()}`}>
+            <Card.Title>{slideItem?.title}</Card.Title>
+            <Card.Entities>
+              {slideItem?.data?.map((item: any) => (
+                <Card.Item key={item?.id} item={item}>
+                  <Card.Image
+                    src={`/images/${category}/${item?.genre}/${item?.slug}/small.jpg`}
+                  />
+                  <Card.Meta>
+                    <Card.SubTitle>{item?.title}</Card.SubTitle>
+                    <Card.Text>{item?.description}</Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+              ))}
+            </Card.Entities>
+            <Card.Feature category={category}>
+              <p>Hello</p>
+            </Card.Feature>
+          </Card>
+        ))}
+      </Card.Group>
     </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
